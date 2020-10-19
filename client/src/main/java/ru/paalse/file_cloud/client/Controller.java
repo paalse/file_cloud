516ef6542dfe2922b87ec4d5dc0fc1a400a96d69
Alex <paalse@list.ru>
2020-10-19 14:41:28 +0400

    Сделаны наброски пользовательского интерфейса

--- client/src/main/java/ru/paalse/file_cloud/client/Controller.java
+++ client/src/main/java/ru/paalse/file_cloud/client/Controller.java
@@ -1,4 +1,10 @@
 package ru.paalse.file_cloud.client;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void exitAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
